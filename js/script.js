142d00d2cdba37fa77f530efffd9c495cb7effa2
Antonius131 <antomendetta@gmail.com>
2022-06-28 14:51:19 +0200

    MILESTONE 1: create teamMembers array

---
+++ js/script.js
@@ -0,0 +1,47 @@
+/**
+ * 
+ *    - Devo creare un array di oggetti che rappresenti i membri del team e le loro caratteristiche
+ *             Wayne Barnett	Founder & CEO	        wayne-barnett-founder-ceo.jpg
+               Angela Caroll	Chief Editor	        angela-caroll-chief-editor.jpg
+               Walter Gordon	Office Manager	        walter-gordon-office-manager.jpg
+               Angela Lopez	Social Media Manager	  angela-lopez-social-media-manager.jpg
+               Scott Estrada	Developer	           scott-estrada-developer.jpg
+               Barbara Ramos	Graphic Designer	     barbara-ramos-graphic-designer.jpg
+
+      - 
+ * 
+ */
+
+
+const teamMembers = [
+   {
+      name: 'Wayne Barrett',
+      role: 'Founder & CEO',
+      image: '../img/wayne-barnett-founder-ceo.jpg'
+   },
+   {
+      name: 'Angela Caroll',
+      role: 'Chief Editor',
+      image: '../img/angela-caroll-chief-editor.jpg'
+   },
+   {
+      name: 'Walter Gordon',
+      role: 'Office Manager',
+      image: '../img/walter-gordon-office-manager.jpg'
+   },
+   {
+      name: 'Angela Lopez',
+      role: 'Social Media Manager',
+      image: '../img/angela-lopez-social-media-manager.jpg'
+   },
+   {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: '../img/scott-estrada-developer.jpg'
+   },
+   {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: '../img/barbara-ramos-graphic-designer.jpg'
+   },
+];
